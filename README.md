620085c4a6dcab3a7583ffde5f52ab8a0114355c
FDMcreative <federico.delmonte@gmail.com>
2018-06-14 12:07:37 +0200

    first upload, lots to do

---
+++ README.md
@@ -0,0 +1,21 @@
+06/06/2018
+####website: [FDMcreative.com](http://www.fdmcreative.com) 
+####email: [federico.delmonte@gmail.com](federico.delmonte@gmail.com)
+---
+# Super Mario Bros 3 - Memory Card Game
+### *Do you remember that card game in that famous videogame? Here it is.*
+-
+
+This project recreates a minigame present in an old Nintendo videogame.
+
+The basic functionalities are all present: selecting the cards, matching, list of items and game over after 3 wrong matches.
+
+-
+
+Difficulties:
+
+- Making the visual size (images) responsive (not implemented yet).
+- Making the stripes background.
+- Making the cards inactive after being clicked.
+- Reverse the inactivity of the 2 selected cards in case they're not a match.
+- Storing 2 cards with the same function in 2 different variables.
---
+++ js/app.js
@@ -0,0 +1,156 @@
+$(() => {
+  // console.log('workoddio');
+
+  //VARIABLES & CONSTANTS
+
+  const $cards = $('.cards');
+  const $card = $('.card');
+  const $start = $('#start');
+  const $reset = $('#reset');
+  const $items = $('#items');
+  const $wrongs = $('#wrongs');
+  const $display = $('#display');
+  let divIndex = 1;
+  let i = 0;
+  let lost = 0;
+  let wrong = 0;
+  let twoPicks = [];
+  let yourItems = [];
+  let storeDivs = [];
+
+  let deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
+  // console.log(deckCards);
+
+  // let randomNumber = Math.floor(Math.random() * deckCards.length);
+
+  //FUNCTIONS
+
+  //This sets the 2 divs back to original state AND reset storeDivs
+  function storeDivsReset () {
+    console.log('wrong')
+    $(storeDivs[0]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
+    $(storeDivs[1]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
+    storeDivs = [];
+  };
+
+  //This Resets the Game
+  function reset() {
+    //This hides the cards
+    $cards.hide();
+    //This resets DISPLAY
+    $display.show();
+    //sets back the ace of spades
+    $card.css({ "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')" });
+    divIndex = 1;
+    $card.text('');
+    deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
+    $card.css({"pointer-events": "auto","background-color":"white"});
+    $card.off();
+    $items.text('');
+    console.log('RESeeET');
+    $wrongs.text(0)
+    wrong = 0;
+    lost = 0;
+    yourItems = [];
+  };
+
+  //This starts the Game
+  function start() {
+
+    //This shows the cards
+    $cards.css({"display":"block"});
+
+    //This sets the Cards on the Table Randomly
+    while (deckCards.length > 0) {
+
+      //This gets a random Element from the card deck and its Index
+      let randomNumber = Math.floor(Math.random() * deckCards.length);
+      let randomCard = deckCards[randomNumber];
+      let indexArray = deckCards.indexOf(deckCards[randomNumber]);
+      // console.log(randomCard, indexArray);
+
+      //removes that Element from the Deck
+      deckCards.splice(indexArray, 1);
+      // console.log('remaining cards: ' + deckCards);
+
+      //puts that Element inside a Div and advances to the next one
+      let div = ('#' + 'card' + divIndex);
+      // console.log(div);
+      $(div).text(randomCard);
+      divIndex++;
+
+    };
+
+    //This Picks a Card
+    $cards.children().on('click', (e) => {
+
+      //This removes the ace of spades
+      $(e.target).css({"background-image": "none"});
+      //This brings the text in the foreground
+      // $(e.target).css({"text-indent": "1px"});
+      //This sets the image
+      let url = ('./images/' + $(e.target).text() + '.jpg');
+      // console.log(url);
+      $(e.target).css({"background-image": "url(" + url + ")"});
+
+      twoPicks[i] = $(e.target).text();
+      $(e.target).css({"pointer-events": "none", "background-color":"grey"})
+
+      console.log('twoPicks: ' + twoPicks);
+      // console.log(twoPicks[1]);
+
+      //This store that Div inside an Array for a possible reset
+      // let x = $(e.target).id;
+      // console.log('x: ' + x);
+      storeDivs[i] = '#' + $(e.target).attr('id');
+      console.log('storeDiv: ' + storeDivs);
+
+      i++;
+
+      //This checks the 2 Cards
+      if (twoPicks[1] !== undefined) {
+        if (twoPicks[0] === twoPicks[1]) {
+          console.log('right');
+          yourItems.push(twoPicks[0]);
+          $items.text(yourItems.join(', '));
+          twoPicks = [];
+          i = 0;
+          storeDivs = [];
+        }
+        else {
+          setTimeout(storeDivsReset, 1000);
+
+          twoPicks = [];
+          i = 0;
+          lost++;
+          wrong++;
+          $wrongs.text(wrong)
+
+        };
+
+        //This checks if 2 lost
+        if (lost === 3) {
+          console.log('LOSssTTT');
+          $display.hide();
+          // $('#display').text('You Lost');
+          $card.css({"pointer-events":"none"});
+          setTimeout(reset, 1000);
+        }
+      }
+
+    });
+  };
+
+  //EVENT LISTENERS
+
+  //This Starts the Game
+  $start.on('click', () => {
+    start();
+  });
+
+  // This Resets the Game
+  $reset.on('click', () =>{
+    reset();
+  });
+
+});
